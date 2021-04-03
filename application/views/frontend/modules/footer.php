9f0391b40d2f0adfa548513239452ce3f4bd4f5b
kevinquang1699 gmail.com <kevinquang1699@gmail.com>
2021-04-04 01:00:58 +0700

    cau truc website

---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,79 @@
+<footer id="footer">
+    
+    <script>!function(s,u,b,i,z){var o,t,r,y;s[i]||(s._sbzaccid=z,s[i]=function(){s[i].q.push(arguments)},s[i].q=[],s[i]("setAccount",z),r=["widget.subiz.net","storage.googleapis"+(t=".com"),"app.sbz.workers.dev",i+"a"+(o=function(k,t){var n=t<=6?5:o(k,t-1)+o(k,t-3);return k!==t?n:n.toString(32)})(20,20)+t,i+"b"+o(30,30)+t,i+"c"+o(40,40)+t],(y=function(k){var t,n;s._subiz_init_2094850928430||r[k]&&(t=u.createElement(b),n=u.getElementsByTagName(b)[0],t.async=1,t.src="https://"+r[k]+"/sbz/app.js?accid="+z,n.parentNode.insertBefore(t,n),setTimeout(y,2e3,k+1))})(0))}(window,document,"script","subiz","acqyptpimeesepcnvtnv")
+    </script>
+
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="#"><i class="fab fa-facebook-f" ></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+              </div>
+        </div>
+    </div>
+    <div class="top-footer">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <a href="#">
+                        <img src="public/images/neshotlogo.png" class="img-fix f-logo" alt="green-construction">
+                    </a>
+                   <div class="about-store">
+                    Cửa hàng hàng bán hàng chuyên cung cấp các sản phẩm chất lượng uy tín.
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="f-contact">
+                        <h3>Thông tin liên hệ</h3>
+                        <ul class="list-unstyled">
+                            <li class="clearfix">
+                                <i class="fa fa-map-marker"></i>
+                                <span>HỒ CHÍ MINH</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-phone"></i>
+                                <span>035698247</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-envelope"></i>
+                                <span><a href="mailto:sale.24hstore@gmail.com">hutech@gmail.com</a></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Chính sách</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="chinh-sach">Chính sách thanh toán</a></li>
+                        <li><a href="chinh-sach">Chính sách vận chuyển</a></li>
+                        <li><a href="chinh-sach">Chính sách đổi trả</a></li>
+                        <li><a href="chinh-sach">Chính sách bảo hành</a></li>          
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Điều khoản</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="dieu-khoan">Điều khoản sử dụng</a></li>           
+                        <li><a href="dieu-khoan">Điều khoản giao dịch</a></li>           
+                        <li><a href="dieu-khoan">Dịch vụ tiện ích</a></li>           
+                        <li><a href="dieu-khoan">Quyền sở hữu trí tuệ</a></li>           
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="payments">
+        <div class="container">
+            <img src="public/images/payment.png" alt="Phương thức thanh toán" class="img-fix">
+        </div>
+    </div>
+    <div class="copyright">
+        © Trường Đại Học Công Nghệ - HUTECH 
+    </div>
+</footer>
